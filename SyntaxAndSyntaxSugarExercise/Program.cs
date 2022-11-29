5c5e255cc13f017ead05af7ceab749530f71b3b5
Kassandra Johnson <kassandra.m.johnson@gmail.com>
2022-11-29 15:10:34 -0500

    completed exercise

--- SyntaxAndSyntaxSugarExercise/Program.cs
+++ SyntaxAndSyntaxSugarExercise/Program.cs
@@ -2,9 +2,11 @@
 {
     public class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello, World!");
+            var answer = 4;
+            var response = (answer < 9) ? $"{answer} is less than nine" : $"{answer} is greater than or equal to nine";
+            Console.WriteLine(response);
         }
     }
 }
